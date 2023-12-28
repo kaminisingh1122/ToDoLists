0dfe7d0604f89dc9295e0bc9782abb3478bf0c30
kamini singh <sengarkamini2002@gmail.com>
2023-12-28 23:03:06 +0530

    first commit

---
+++ Downloads/Campus-Placement-Information--Training-Portal--Client--main (1)/campus-placement/src/components/Form/styles.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+    
+    borderRadius:"7px",
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+    backgroundColor:"black",
+    color:"white",
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+    color:"white",
+    backgroundColor:"#3ef238",
+    borderRadius:"6px",
+  },
+  btn:{
+    backgroundColor:"#f24933",
+  },
+}));
